face92ffa295acefc97fca3555b75b47724aa47a
kispekorea <kispekorea@gmail.com>
2026-05-01 23:46:39 +0900

    Add files via upload

---
+++ minimal-daily-log/minimal-daily-log/minimal-daily-log/src/firebase.ts
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// 파일에서 불러오지 않고 직접 설정값을 넣었습니다.
+const firebaseConfig = {
+  projectId: "gen-lang-client-0327863758",
+  appId: "1:1080163261323:web:1e3b6712c9ed1bfefdad8e",
+  apiKey: "AIzaSyDSFMQHUi-Qast7WWwaAi0vD9NSvyKdFEU",
+  authDomain: "gen-lang-client-0327863758.firebaseapp.com",
+  storageBucket: "gen-lang-client-0327863758.firebasestorage.app",
+  messagingSenderId: "1080163261323"
+};
+
+const databaseId = "ai-studio-9b041de8-25f1-45e8-b8d2-8888800a5f71a";
+
+const app = initializeApp(firebaseConfig);
+// 여기서 아까 그 긴 데이터베이스 아이디를 바로 연결합니다.
+export const db = getFirestore(app, databaseId);
+export const auth = getAuth(app);
+
+export enum OperationType {
+  CREATE = 'create',
+  UPDATE = 'update',
+  DELETE = 'delete',
+  LIST = 'list',
+  GET = 'get',
+  WRITE = 'write',
+}
