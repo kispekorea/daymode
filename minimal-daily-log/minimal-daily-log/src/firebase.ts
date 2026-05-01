d1c8196d17aec031758f960db55f182e81f0590a
kispekorea <kispekorea@gmail.com>
2026-05-02 00:02:38 +0900

    Add files via upload

--- minimal-daily-log/minimal-daily-log/src/firebase.ts
+++ minimal-daily-log/minimal-daily-log/src/firebase.ts
@@ -2,20 +2,19 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDSFMQHUi-Qast7WWwaAi0vD9NSvYkDFeU",
-  authDomain: "gen-lang-client-0327863758.firebaseapp.com",
-  projectId: "gen-lang-client-0327863758",
-  storageBucket: "gen-lang-client-0327863758.firebasestorage.app",
-  messagingSenderId: "1080163261323",
-  appId: "1:1080163261323:web:1e3b6712c9ed1bfefdad8e"
+let app;
+let auth;
+let db;
+
+// 앱이 실행될 때 json 파일을 읽어서 여기로 던져줄 겁니다.
+export const initFirebase = (config) => {
+  app = initializeApp(config);
+  auth = getAuth(app);
+  db = getFirestore(app);
+  return { app, auth, db };
 };
 
-// 데이터베이스 ID는 일단 기본값(default)으로 돌려놓겠습니다. 
-// 로그인이 먼저 되어야 데이터고 뭐고 확인이 가능하니까요!
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app); 
-export const auth = getAuth(app);
+export { auth, db };
 
 export enum OperationType {
   CREATE = 'create',
